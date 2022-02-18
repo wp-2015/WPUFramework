ccd1cfc6cbb4f91913e2191d154cfd66b9638650
王鹏 <>
2022-02-18 16:24:33 +0800

    ABHelper

---
+++ ABHelper/ABHelper/Assets/AssetManager.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace ABHelper
+{
+    public class AssetManager
+    {
+        private static bool bIsUseAssetBundle;
+        public AssetManager()
+        {
+            bIsUseAssetBundle = IsUseAssetBundle;
+        }
+//        public static UnityEngine.Object Load(string path)
+//        {
+//#if UNITY_EDITOR
+//            if(!IsUseAssetBundle)
+//            {
+//                 return Editor.Instantiate<GameObject>(AssetDatabase.LoadAssetAtPath<GameObject>(path));
+//            }
+//#endif
+//            return ABManager.Load(path);
+//        }
+
+        public static T Load<T>(string path) where T : Object
+        {
+#if UNITY_EDITOR
+            if (!bIsUseAssetBundle)
+            {
+                return (T)AssetDatabase.LoadAssetAtPath<T>(path);
+            }
+#endif
+            return ABManager.Load<T>(path);
+        }
+
+        public static void UnLoad(string path)
+        {
+#if UNITY_EDITOR
+            if (bIsUseAssetBundle)
+#endif
+            {
+                ABManager.UnLoad(path);
+            }
+        }
+
+        public static void UnLoadAllUnuseAsset()
+        {
+#if UNITY_EDITOR
+            if (bIsUseAssetBundle)
+#endif
+            {
+                ABManager.UnLoadUnusefulAssetBundle();
+            }
+        }
+
+        private static bool IsUseAssetBundle
+        {
+            get
+            {
+                return PlayerPrefs.GetString("UseAssetBundle") == "ON";
+            }
+            set
+            {
+                string res = value ? "ON" : "OFF";
+                PlayerPrefs.SetString("UseAssetBundle", res);
+            }
+        }
+#if UNITY_EDITOR
+        [MenuItem("ABHelper/资源加载方式/使用AssetBundle", true)]
+        static bool UseBundle()
+        {
+            return !IsUseAssetBundle && !Application.isPlaying;
+        }
+        [MenuItem("ABHelper/资源加载方式/使用AssetBundle", false)]
+        static void UseBundleShow()
+        {
+            IsUseAssetBundle = true;
+        }
+        [MenuItem("ABHelper/资源加载方式/不使用AssetBundle", true)]
+        static bool NotUseBundle()
+        {
+            return IsUseAssetBundle && !Application.isPlaying;
+        }
+        [MenuItem("ABHelper/资源加载方式/不使用AssetBundle", false)]
+        static void NotUseBundleShow()
+        {
+            IsUseAssetBundle = false;
+        }
+#endif
+    }
+}
---
+++ ABHelper/ABHelper/Editor/BuildScript.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace ABHelper
+{
+    public enum TargetPlatform { Window = BuildTarget.StandaloneWindows64, Android = BuildTarget.Android, IOS = BuildTarget.iOS }
+    public partial class BuildScript
+    {
+        public static void CheckPlatformAndBuild()
+        {
+            var abManifest = AssetsUtils.GetAssetFile<ABManifest>(Config.ABManifestAssetPath);
+            if ((int)abManifest.BuildTargetPlatform == (int)EditorUserBuildSettings.activeBuildTarget)
+            {
+                Build();
+            }
+            else
+            {
+                EditorUserBuildSettings.activeBuildTargetChanged = delegate ()
+                {
+                    Build();
+                };
+                EditorUserBuildSettings.SwitchActiveBuildTarget(abManifest.TargetGroup, (BuildTarget)abManifest.BuildTargetPlatform);
+            }
+        }
+
+        public static void Build()
+        {
+            EditorUtility.DisplayProgressBar("ABHelper", "重新生成配置文件", 0);
+            //打包之前需要确定manifest文件
+            MakeABManifest();
+            EditorUtility.DisplayProgressBar("ABHelper", "重新生成配置文件", 1);
+            var abManifest = AssetsUtils.GetAssetFile<ABManifest>(Config.ABManifestAssetPath);
+            //检查配置文件的有效行
+            abManifest.CheckAssetDatasValid();
+
+            EditorUtility.DisplayProgressBar("ABHelper", "读取AssetFileInfo文件", 1);
+
+            //统计所有需要打包的资源文件
+            var assets = abManifest.AssetDatas;
+            var dirs = abManifest.Dirs;
+            var abName = abManifest.BundleNames;
+            var map = new Dictionary<string, List<string>>();
+            var len = assets.Length;
+            for (int i = 0; i < len; i++)
+            {
+                var asset = assets[i];
+                var bundleName = abName[asset.BundleNameInde];
+                var dir = dirs[asset.DirIndex];
+                var path = dir + "/" + asset.Name;
+                List<string> assetToBuild;
+                if (!map.TryGetValue(bundleName, out assetToBuild))
+                {
+                    assetToBuild = new List<string>();
+                    map[bundleName] = assetToBuild;
+                }
+                assetToBuild.Add(path);
+            }
+            var waitforBuild = new Dictionary<string, List<string>>();
+            foreach (var item in map)
+            {
+                var bundleName = item.Key;
+                var assetToBuild = item.Value;
+                waitforBuild[bundleName] = assetToBuild;
+            }
+
+            //需要打包的资源文件放入buildMap中
+            List<AssetBundleBuild> builds = new List<AssetBundleBuild>();
+            foreach (var ab in waitforBuild)
+            {
+                foreach(var a in ab.Value)
+                {
+                    Debug.Log(ab.Key + "    " + a);
+                }
+                builds.Add(new AssetBundleBuild()
+                {
+                    assetBundleName = ab.Key,
+                    assetNames = ab.Value.ToArray()
+                });
+            }
+
+            EditorUtility.DisplayProgressBar("ABHelper", "开始打包", 1);
+            //调用打包接口
+            var outputFolder = abManifest.OutputPlatformFolder;//PathUtils.MakeAbsolutePath(abManifest.OutputFolder, Application.dataPath);
+
+            //TODO:暂时先删除以前的所有AB
+            if (Directory.Exists(outputFolder))
+                Directory.Delete(outputFolder, true);
+
+            PathUtils.ConfirmDirectoryExist(outputFolder);
+
+            var manifest = BuildPipeline.BuildAssetBundles(outputFolder, builds.ToArray(), 
+                                abManifest.BuildOptions, EditorUserBuildSettings.activeBuildTarget);
+
+            EditorUtility.DisplayProgressBar("ABHelper", "生成本次打包记录文件", 1);
+
+            if (EditorUtility.DisplayDialog("ABHelper", "AssetBundle打包完成, 是否需要打开输出文件", "打开", "取消"))
+            {
+                EditorUtility.OpenWithDefaultApp(outputFolder);
+            }
+            EditorUtility.ClearProgressBar();
+        }
+
+        /// <summary>
+        /// 需要打包的文件是否合法(是否需要过滤的)
+        /// </summary>
+        public static bool IsFileValid(string fileName, string[] ignoreList)
+        {
+            for(int i = 0; i < ignoreList.Length; i++)
+            {
+                if(fileName.EndsWith(ignoreList[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
